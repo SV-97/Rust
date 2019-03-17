24dd069b6a9b4e6a2f0eacad4e882c0fa40516f3
Stefan Volz <volzstefan97@googlemail.com>
2019-03-17 12:58:05 +0100

    Renamed to fit naming scheme

--- ls/target/rls/debug/deps/ls-9baaad07ff94c30c.d
+++
@@ -1,5 +0,0 @@
-/home/sv-97/GitHub/Rust/ls/target/rls/debug/deps/ls-9baaad07ff94c30c.rmeta: src/main.rs
-
-/home/sv-97/GitHub/Rust/ls/target/rls/debug/deps/ls-9baaad07ff94c30c.d: src/main.rs
-
-src/main.rs:
